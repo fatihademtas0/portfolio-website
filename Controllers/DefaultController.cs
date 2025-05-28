2ed9889a192cf6b8708fdcc60c20b71215581dc7
fatihademtas0 <fatihademtas@outlook.com>
2025-05-28 03:43:03 +0300

    creating models and db connections

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvProject.Models.Entity;
+
+namespace MvcCvProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+		DbCvEntities2 db = new DbCvEntities2();
+		public ActionResult Index()
+        {
+            var values = db.TblAbout.ToList();		
+			return View(values);
+        }
+
+        public PartialViewResult Experiences()
+        {
+            var values = db.TblExperiences.ToList();
+            return PartialView(values);
+        }
+
+		public PartialViewResult Projects()
+		{
+			var values = db.TblProjects.ToList();
+			return PartialView(values);
+		}
+
+		public PartialViewResult Education()
+		{
+			var values = db.TblEducation.ToList();
+			return PartialView(values);
+		}
+	}
+}
