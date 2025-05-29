8c844bc5f46203b11b35c7433d629fed0cd8ece3
fatihademtas0 <fatihademtas@outlook.com>
2025-05-29 03:46:25 +0300

    experience panel

--- Controllers/ExperienceController.cs
+++ Controllers/ExperienceController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcCvProject.Models.Entity;
+using MvcCvProject.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +8,52 @@ using System.Web.Mvc;
 
 namespace MvcCvProject.Controllers
 {
-    public class ExperienceController : Controller
-    {
-        // GET: About
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class ExperienceController : Controller
+	{
+		ExperienceRepository repo = new ExperienceRepository();
+		public ActionResult Index()
+		{
+			var values = repo.List();
+			return View(values);
+		}
+
+		[HttpGet]
+		public ActionResult AddExperience()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult AddExperience(TblExperiences t)
+		{
+			repo.TAdd(t);
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult DeleteExperience(int id)
+		{
+			TblExperiences t = repo.Find(x => x.id == id);
+			repo.TRemove(t);
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public ActionResult GetExperience(int id)
+		{
+			TblExperiences experience = repo.Find(x => x.id == id);
+			return View(experience);
+		}
+
+		[HttpPost]
+		public ActionResult GetExperience(TblExperiences t)
+		{
+			TblExperiences experience = repo.Find(x => x.id == t.id);
+			experience.title = t.title;
+			experience.subtitle = t.subtitle;
+			experience.description = t.description;
+			experience.date = t.date;
+			repo.TUpdate(experience);
+			return RedirectToAction("Index");
+		}
+	}
 }
---
+++ Repositories/ExperienceRepository.cs
@@ -0,0 +1,17 @@
+﻿using MvcCvProject.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCvProject.Repositories
+{
+	public class ExperienceRepository : GenericRepository<TblExperiences>
+	{
+		DbCvEntities2 db = new DbCvEntities2();
+		public List<TblExperiences> List()
+		{
+			return db.TblExperiences.ToList();
+		}
+	}
+}
