0f8967f2afa91cdb42cf59890ce3fb95b6f11bc7
fatihademtas0 <fatihademtas@outlook.com>
2025-05-31 04:27:40 +0300

    admin panel projects

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvProject.Models.Entity;
+using MvcCvProject.Repositories;
+
+namespace MvcCvProject.Controllers
+{
+    public class CourseController : Controller
+    {
+        GenericRepository<TblCourses> repo = new GenericRepository<TblCourses>();
+
+        public ActionResult Index()
+        {
+            var courses = repo.List();
+            return View(courses);
+        }
+
+        [HttpGet]
+        public ActionResult AddCourse()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCourse(TblCourses course)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.TAdd(course);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("AddCourse");
+            }
+        }
+
+        public ActionResult DeleteCourse(int id)
+		{
+			TblCourses course = repo.Find(x => x.id == id);
+			if (course != null)
+			{
+				repo.TRemove(course);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return HttpNotFound();
+			}
+		}
+
+		[HttpGet]
+		public ActionResult UpdateCourse(int id)
+		{
+			TblCourses course = repo.Find(x => x.id == id);
+			if(course != null)
+			{
+				return View(course);
+			}else
+			{
+				return HttpNotFound();
+			}
+		}
+
+		[HttpPost]
+		public ActionResult UpdateCourse(TblCourses course)
+		{
+			if(course != null && ModelState.IsValid)
+			{
+				TblCourses cou = repo.Find(x => x.id == course.id);
+
+				if (cou != null)
+				{
+					cou.description = course.description;
+					repo.TUpdate(cou);
+					return RedirectToAction("Index");
+				}
+				else
+				{
+					return RedirectToAction("UpdateCourse");
+				}
+			}else
+			{
+				return RedirectToAction("UpdateCourse");
+			}
+		}
+	}
+}
--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -10,10 +10,79 @@ namespace MvcCvProject.Controllers
 {
     public class ProjectController : Controller
     {
-        // GET: About
-        public ActionResult Index()
+
+		GenericRepository<TblProjects> repo = new GenericRepository<TblProjects>();
+		public ActionResult Index()
+        {
+            var projects = repo.List();
+            return View(projects);
+        }
+
+        [HttpGet]
+        public ActionResult AddProject()
         {
             return View();
         }
-    }
+
+        [HttpPost]
+        public ActionResult AddProject(TblProjects project)
+        {
+			if (ModelState.IsValid)
+			{
+				repo.TAdd(project);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View("AddProject");
+			}
+		}
+
+        public ActionResult DeleteProject(int id)
+        {
+            TblProjects pro = repo.Find(x => x.id == id);
+			if (pro != null)
+			{
+				repo.TRemove(pro);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return HttpNotFound();
+			}
+		}
+
+		[HttpGet]
+        public ActionResult UpdateProject(int id)
+        {
+			TblProjects pro = repo.Find(x => x.id == id);
+			if (pro != null)
+			{
+				return View(pro);
+			}else
+			{
+				return HttpNotFound();
+			}
+
+		}
+
+		[HttpPost]
+		public ActionResult UpdateProject(TblProjects pro)
+		{
+			TblProjects project = repo.Find(x => x.id == pro.id);
+			if (project != null)
+			{
+				project.title = pro.title;
+				project.description = pro.description;
+				project.tools = pro.tools;
+				repo.TUpdate(project);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View("UpdateProject");
+
+			}
+		}
+	}
 }
