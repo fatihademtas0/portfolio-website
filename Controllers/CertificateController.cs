dfbcae1d613b50b3c3e65c5384f303f720ae323d
fatihademtas0 <fatihademtas@outlook.com>
2025-06-25 18:04:59 +0300

    ınterests

--- Controllers/CertificateController.cs
+++ Controllers/CertificateController.cs
@@ -3,15 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCvProject.Models.Entity;
+using MvcCvProject.Repositories;
 
 namespace MvcCvProject.Controllers
 {
     public class CertificateController : Controller
     {
-        // GET: About
+        GenericRepository<TblCertfications> repo = new GenericRepository<TblCertfications>();
         public ActionResult Index()
         {
-            return View();
+            var certificates = repo.List();
+			return View(certificates);
         }
-    }
+
+		public ActionResult DeleteCertificate(int id)
+		{
+			TblCertfications course = repo.Find(x => x.id == id);
+			if (course != null)
+			{
+				repo.TRemove(course);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return HttpNotFound();
+			}
+		}
+
+		[HttpGet]
+		public ActionResult GetCertificate(int id)
+		{
+			var certificate = repo.Find(x => x.id == id);
+			return View(certificate);
+		}
+
+		[HttpPost]
+		public ActionResult GetCertificate(TblCertfications t)
+		{
+			var certificate = repo.Find(x => x.id == t.id);
+			certificate.description = t.description;
+			repo.TUpdate(certificate);
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public ActionResult AddCertificate()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult AddCertificate(TblCertfications t)
+		{
+			repo.TAdd(t);
+			return RedirectToAction("Index");
+		}
+	}
 }
--- Controllers/InterestController.cs
+++ Controllers/InterestController.cs
@@ -3,15 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCvProject.Models.Entity;
+using MvcCvProject.Repositories;
 
 namespace MvcCvProject.Controllers
 {
     public class InterestController : Controller
     {
-        // GET: About
-        public ActionResult Index()
+		GenericRepository<TblInterests> repo = new GenericRepository<TblInterests>();
+		public ActionResult Index()
         {
-            return View();
+			var interests = repo.List();
+			return View();
         }
-    }
+
+		public ActionResult DeleteInterest(int id)
+		{
+			TblInterests interest = repo.Find(x => x.id == id);
+			if (interest != null)
+			{
+				repo.TRemove(interest);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return HttpNotFound();
+			}
+		}
+
+		[HttpGet]
+		public ActionResult UpdateInterest(int id)
+		{
+			var interest = repo.Find(x => x.id == id);
+			return View(interest);
+		}
+
+		[HttpPost]
+		public ActionResult UpdateInterest(TblInterests t)
+		{
+			var interest= repo.Find(x => x.id == t.id);
+			interest.description1 = t.description1;
+			interest.description2 = t.description2;
+			repo.TUpdate(interest);
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public ActionResult AddInterest()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult AddInterest(TblInterests t)
+		{
+			repo.TAdd(t);
+			return RedirectToAction("Index");
+		}
+	}
 }
