977cb413bfca090f0d75f406770473c582a8ca87
fatihademtas0 <fatihademtas@outlook.com>
2025-07-01 19:28:24 +0300

    finale

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -8,12 +8,51 @@ using MvcCvProject.Repositories;
 
 namespace MvcCvProject.Controllers
 {
-    public class AdminController : Controller
-    {
-        // GET: Admin
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	[Authorize]
+	public class AdminController : Controller
+	{
+		GenericRepository<TblAdmin> repo = new GenericRepository<TblAdmin>();
+		public ActionResult Index()
+		{
+			var value = repo.List();
+			return View(value);
+		}
+
+		[HttpGet]
+		public ActionResult Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Add(TblAdmin t)
+		{
+			repo.TAdd(t);
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult Delete(int id)
+		{
+			TblAdmin t = repo.Find(x => x.id == id);
+			repo.TRemove(t);
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public ActionResult Update(int id)
+		{
+			TblAdmin user = repo.Find(x => x.id == id);
+			return View(user);
+		}
+
+		[HttpPost]
+		public ActionResult Update(TblAdmin t)
+		{
+			TblAdmin user = repo.Find(x => x.id == t.id);
+			user.username= t.username;
+			user.password= t.password;
+			repo.TUpdate(user);
+			return RedirectToAction("Index");
+		}
+	}
 }
